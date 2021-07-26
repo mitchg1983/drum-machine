54e18fd25e76688d58458ddd44b99d4125d04945
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2021-07-26 12:09:13 -0400

    Add 4th beat tock

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 // Setup 'tick' sound
-const tick = new Audio('sounds/tick.mp3');
+const tick = new Audio("sounds/tick.mp3");
 
+const tock = new Audio("sounds/tock.mp3");
+
+let count = 1;
 // This function is called every 600ms
 function update() {
 
-    // Play the 'tick' sound
+    if (count <= 3) {
+        console.log(count);
     tick.play();
+    count++; 
+    return
+
+  }
+
+  if (count === 4) {
+    console.log(count);
+    tock.play();
+    count = 1;
+    return
+  }
 }
 
 // This function sets up update() to be called every 600ms
 function setupUpdate() {
-    setInterval(update, 600);
+  setInterval(update, 600);
 }
 
 // Call setupUpdate() once after 300ms
